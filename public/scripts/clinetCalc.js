9064e5e638af1f31e80fe0f12dcee705bac79a8b
ngorbunov <nick_g@outlook.com>
2016-06-13 13:59:22 -0500

    add more to branch

--- public/scripts/clinetCalc.js
+++ public/scripts/clinetCalc.js
@@ -1,1 +1,35 @@
 console.log('clientcalc.js is sourced');
+var operation = "add";
+$(document).ready(function(){
+    console.log("jQuery is here!");
+    $('#calcButton').on('click', function(){
+        console.log("calculate button pressed");
+        // get variable inputs
+        var x = $('#input1').val();
+        var y = $('#input2').val();
+        console.log("x = " + x + " y = " + y);
+        // assemble input into object
+        var inputObject = {
+          "x": x,
+          "y": y,
+          "operation": operation
+        };
+
+        //changing operation global variable based on which button was clicked
+        $('#addButton').on('click', function(){
+          operation = "add";
+        });
+        $('#subtractButton').on('click', function(){
+          operation = "subtract";
+        });
+        $('#multiplyButton').on('click', function(){
+          operation = "multiply";
+        });
+        $('#divideButton').on('click', function(){
+          operation = "divide";
+        });
+
+        console.table("input object x " + inputObject.x + " input object y " + inputObject.y + " operation: " + operation);
+  });
+
+});
