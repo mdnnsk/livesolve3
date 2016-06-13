22b4a58616df154b881340cbea2fd716c38b02ed
ngorbunov <nick_g@outlook.com>
2016-06-13 13:24:12 -0500

    ver0_2

---
+++ public/scripts/clinetCalc.js
@@ -0,0 +1,1 @@
+console.log('clientcalc.js is sourced');
--- server/app.js
+++ server/app.js
@@ -3,13 +3,20 @@ var express = require ('express');
 
 var app = express();
 
+var path = require ('path');
+
+// spin up server
 app.listen( 3000, 'localhost', function(){
-  var port = server.address().port;
-    console.log('server is listening on ' + port);
+  // var port = server.address().port;
+    console.log('server is listening on port 3000');
 });
 // base url
 app.get('/', function(req,res){
+  // server side logs show up in terminal
   console.log('in base url');
-  res.write("this is the home page");
-  res.end();
+  // write something on the page
+  res.sendFile( path.resolve( 'views/index.html'));
 });
+
+// set up a folder for static files
+app.use(express.static('public'));
